26516dfb2bd2768e34bff3b304df31da84c9feab
riccorichards <richard.topimovi.1@iliauni.edu.ge>
2024-09-11 23:44:10 +0400

    update discount url

--- src/database/product.repo.ts
+++ src/database/product.repo.ts
@@ -5,10 +5,10 @@ class ProductRepository {
   async GetDiscountProducts(limit = 9) {
     const wines = await WineModel.find({ discount: { $gt: 0 } })
       .limit(limit)
-      .select("titleTranslations price discount image");
+      .select("titleTranslations price discount url");
     const coctails = await CocktailModel.find({ discount: { $gt: 0 } })
       .limit(limit)
-      .select("titleTranslations price discount image");
+      .select("titleTranslations price discount url");
 
     return [...wines, ...coctails];
   }
@@ -16,11 +16,11 @@ class ProductRepository {
   async GetMostSoldProducts(limit = 3, page = 0) {
     const wines = await WineModel.find()
       .sort({ orderCourt: -1 })
-      .select("titleTranslations price discount image orderCount")
+      .select("titleTranslations price discount url orderCount")
       .skip(page * limit);
     const coctails = await CocktailModel.find()
       .sort({ orderCourt: -1 })
-      .select("titleTranslations price discount image orderCount")
+      .select("titleTranslations price discount url orderCount")
       .skip(page * limit);
     return [...wines, ...coctails]
       .slice(page * limit, 5)
